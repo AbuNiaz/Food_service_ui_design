d2fbd8c52f9936c6ec5e7adc98c5ff4a5311377c
Abu Niaz <niaz35-507@diu.edu.bd>
2022-03-27 12:41:03 +0600

    Update header.dart

--- food_sevice_ui/lib/header.dart
+++ food_sevice_ui/lib/header.dart
@@ -12,39 +12,76 @@ class _HeaderState extends State<Header> {
     return SliverList(
       delegate: SliverChildListDelegate(
         [
-          Column(
+          Stack(
             children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                height: size.height / 5,
-                decoration: const BoxDecoration(
-                  color: Colors.teal,
-                  borderRadius: BorderRadius.vertical(
-                    bottom: Radius.circular(40),
-                  ),
-                  boxShadow: [
-                    BoxShadow(blurRadius: 2),
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      children: const [
-                        CircleAvatar(
-                          backgroundColor: Colors.white70,
-                          radius: 30,
-                          child: CircleAvatar(
-                            backgroundImage: AssetImage("images/g5.jpg"),
-                            radius: 28,
-                          ),
-                        ),
+              Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    height: size.height / 5,
+                    decoration: const BoxDecoration(
+                      color: Colors.teal,
+                      borderRadius: BorderRadius.vertical(
+                        bottom: Radius.circular(40),
+                      ),
+                      boxShadow: [
+                        BoxShadow(blurRadius: 2),
                       ],
-                    )
-                  ],
-                ),
+                    ),
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            const CircleAvatar(
+                              backgroundColor: Colors.white70,
+                              radius: 30,
+                              child: CircleAvatar(
+                                backgroundImage: AssetImage("images/g5.jpg"),
+                                radius: 28,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            Column(
+                              children: [
+                                const Text(
+                                  "User Name",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(4),
+                                  decoration: BoxDecoration(
+                                    color: Colors.black54,
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: const Text(
+                                    "GOLD Member",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const Text(
+                              "154 \$ CAN",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
