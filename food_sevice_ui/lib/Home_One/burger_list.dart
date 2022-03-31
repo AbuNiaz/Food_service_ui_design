1d397a26e31ed90fe2cd7751e75054eb96559055
Abu Niaz <niaz35-507@diu.edu.bd>
2022-03-31 11:26:29 +0600

    Update burger_list.dart

--- food_sevice_ui/lib/Home_One/burger_list.dart
+++ food_sevice_ui/lib/Home_One/burger_list.dart
@@ -10,14 +10,39 @@ class BurgerList extends StatefulWidget {
 class _BurgerListState extends State<BurgerList> {
   @override
   Widget build(BuildContext context) {
+    int item = 10;
     return SliverToBoxAdapter(
       child: Container(
+        height: 240,
+        margin: const EdgeInsets.only(top: 15),
         child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: item,
           itemBuilder: (context, index) => Stack(
             children: [
               Container(
-                child: Card(
-                  child: Text("Burger"),
+                height: 240,
+                width: 200,
+                margin:
+                    EdgeInsets.only(left: 15, right: index == item ? 15 : 0),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 15),
+                      child: Column(
+                        children: const [
+                          Text(
+                            "Burger",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ],
