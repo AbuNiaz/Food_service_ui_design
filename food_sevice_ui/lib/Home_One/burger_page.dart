6f3488ac333d08f2705d334cd8a39b8437bd4ecc
Abu Niaz <niaz35-507@diu.edu.bd>
2022-04-03 07:23:35 +0600

    Create burger_page.dart

---
+++ food_sevice_ui/lib/Home_One/burger_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BurgerPage extends StatefulWidget {
+  const BurgerPage({Key? key}) : super(key: key);
+
+  @override
+  State<BurgerPage> createState() => _BurgerPageState();
+}
+
+class _BurgerPageState extends State<BurgerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
