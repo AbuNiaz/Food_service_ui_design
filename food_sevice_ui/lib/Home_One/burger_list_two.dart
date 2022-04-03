1123afcdf4173d32876c7b5b32a8ec804048ab10
Abu Niaz <niaz35-507@diu.edu.bd>
2022-04-03 06:32:16 +0600

    Update burger_list_two.dart

--- food_sevice_ui/lib/Home_One/burger_list_two.dart
+++ food_sevice_ui/lib/Home_One/burger_list_two.dart
@@ -41,88 +41,101 @@ class _BurgerListState extends State<BurgerListTwo> {
     //
 
     return SliverToBoxAdapter(
-      child: Container(
-        height: 240,
-        margin: const EdgeInsets.only(top: 0, left: 10),
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: item,
-          itemBuilder: (context, index) {
-            bool reverse = index.isEven;
-            return Stack(
-              children: [
-                Container(
-                  height: 220,
-                  width: 160,
-                  margin: const EdgeInsets.only(left: 15, bottom: 40, top: 5),
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: Card(
-                      color: Theme.of(context).primaryColor,
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                          top: 10,
-                        ),
-                        child: Column(
-                          children: [
-                            Text(
-                              reverse ? "Chicken Burger" : "Simple Burger",
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
-                              ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Most Pupoler:",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            height: 240,
+            margin: const EdgeInsets.only(top: 0, left: 10),
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: item,
+              itemBuilder: (context, index) {
+                bool reverse = index.isEven;
+                return Stack(
+                  children: [
+                    Container(
+                      height: 220,
+                      width: 160,
+                      margin:
+                          const EdgeInsets.only(left: 15, bottom: 40, top: 5),
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Card(
+                          color: Theme.of(context).primaryColor,
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                              top: 10,
                             ),
-                            const Spacer(),
-                            Row(
+                            child: Column(
                               children: [
-                                const Spacer(),
-                                const Text(
-                                  "150 BDT",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                const Spacer(),
-                                Container(
-                                  height: 40,
-                                  width: 40,
-                                  child: Card(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(10)),
-                                    child: const Icon(Icons.add),
+                                Text(
+                                  reverse ? "Chicken Burger" : "Simple Burger",
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
                                   ),
                                 ),
+                                const Spacer(),
+                                Row(
+                                  children: [
+                                    const Spacer(),
+                                    const Text(
+                                      "150 BDT",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const Spacer(),
+                                    Container(
+                                      height: 40,
+                                      width: 40,
+                                      child: Card(
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                        child: const Icon(Icons.add),
+                                      ),
+                                    ),
+                                  ],
+                                )
                               ],
-                            )
-                          ],
-                        ),
-                      ),
-                      elevation: 3,
-                      margin: const EdgeInsets.all(10),
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(45),
-                          bottomRight: Radius.circular(15),
-                          topLeft: Radius.circular(45),
-                          topRight: Radius.circular(45),
+                            ),
+                          ),
+                          elevation: 3,
+                          margin: const EdgeInsets.all(10),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(45),
+                              bottomRight: Radius.circular(15),
+                              topLeft: Radius.circular(45),
+                              topRight: Radius.circular(45),
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ),
-                Positioned(
-                  top: reverse ? 48 : 48,
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: reverse ? chickenBurder : simpleBurger,
-                  ),
-                ),
-              ],
-            );
-          },
-        ),
+                    Positioned(
+                      top: reverse ? 48 : 48,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: reverse ? chickenBurder : simpleBurger,
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
