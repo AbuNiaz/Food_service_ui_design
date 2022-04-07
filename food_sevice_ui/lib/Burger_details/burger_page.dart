6c971cb91d866cd5d71a339a666ab61490d8efe6
Abu Niaz <niaz35-507@diu.edu.bd>
2022-04-07 10:38:07 +0600

    Update burger_page.dart

--- food_sevice_ui/lib/Burger_details/burger_page.dart
+++ food_sevice_ui/lib/Burger_details/burger_page.dart
@@ -14,26 +14,29 @@ class BurgerPage extends StatelessWidget {
     BuildContext context,
   ) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-          leading: IconButton(
-              onPressed: () {
-                Navigator.of(context);
-              },
-              icon: const Icon(Icons.arrow_back_ios_new_outlined)),
-          elevation: 0,
-        ),
-        body: Stack(
-          children: [
-            Container(
-              height: 200,
-              decoration: const BoxDecoration(
-                  color: Colors.teal,
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(50),
-                      bottomRight: Radius.circular(50))),
-            )
-          ],
-        ));
+      appBar: AppBar(
+        title: Text(title),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context);
+            },
+            icon: const Icon(Icons.arrow_back_ios_new_outlined)),
+        elevation: 0,
+      ),
+      body: Stack(
+        children: [
+          Container(
+            height: 200,
+            decoration: const BoxDecoration(
+              color: Colors.teal,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(50),
+                bottomRight: Radius.circular(50),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
