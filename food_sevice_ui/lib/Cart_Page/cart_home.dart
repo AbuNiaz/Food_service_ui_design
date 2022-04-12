8c4cb8eccc67fc649cc3c9a1585f2854df4bc258
Abu Niaz <niaz35-507@diu.edu.bd>
2022-04-12 10:44:40 +0600

    Update cart_home.dart

--- food_sevice_ui/lib/Cart_Page/cart_home.dart
+++ food_sevice_ui/lib/Cart_Page/cart_home.dart
@@ -5,6 +5,39 @@ class CartHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cart"),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.notifications))
+        ],
+        elevation: 0,
+      ),
+      body: Container(
+        height: 500,
+        width: double.infinity,
+        child: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (context, index) => Stack(
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          height: 40,
+                          width: double.infinity,
+                          decoration: const BoxDecoration(
+                              color: Colors.red,
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                "images/b5.jpg",
+                              ))),
+                        )
+                      ],
+                    )
+                  ],
+                )),
+      ),
+    );
   }
 }
