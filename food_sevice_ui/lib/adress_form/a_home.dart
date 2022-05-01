a563d090a95bc9fa8186aea5fea5692b5b07ba5c
Abu Niaz <niaz35-507@diu.edu.bd>
2022-05-01 09:04:58 +0600

    Create a_home.dart

---
+++ food_sevice_ui/lib/adress_form/a_home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdressHome extends StatelessWidget {
+  const AdressHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
