b096a664df72dde7a824764b8ac5c4a626d81cb8
Abu Niaz <niaz35-507@diu.edu.bd>
2022-05-04 20:23:00 +0600

    Update main.dart

--- food_sevice_ui/lib/main.dart
+++ food_sevice_ui/lib/main.dart
@@ -20,8 +20,21 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
         ),
         bottomAppBarColor: Colors.teal,
-        floatingActionButtonTheme:
-            const FloatingActionButtonThemeData(backgroundColor: Colors.teal),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Colors.teal,
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20), gapPadding: 4),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: const BorderSide(color: Colors.blue),
+              gapPadding: 4,
+            ),
+            focusColor: Colors.blue),
       ),
       home: const Hamberger(),
     );
