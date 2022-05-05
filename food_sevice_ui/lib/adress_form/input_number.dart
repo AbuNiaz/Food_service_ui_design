25840d2a25d27815521361dba6446fa7f20ee97f
Abu Niaz <niaz35-507@diu.edu.bd>
2022-05-05 21:42:06 +0600

    Create input_number.dart

---
+++ food_sevice_ui/lib/adress_form/input_number.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NumberRow extends StatelessWidget {
+  const NumberRow({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [],
+    );
+  }
+}
