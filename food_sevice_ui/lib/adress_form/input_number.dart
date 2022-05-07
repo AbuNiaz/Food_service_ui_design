83f419e2ba381a949c8593c2e6e2aeb85100018b
Abu Niaz <niaz35-507@diu.edu.bd>
2022-05-07 19:23:35 +0600

    Update input_number.dart

--- food_sevice_ui/lib/adress_form/input_number.dart
+++ food_sevice_ui/lib/adress_form/input_number.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 
-class NumberRow extends StatelessWidget {
-  const NumberRow({
-    Key? key,
-  }) : super(key: key);
+class NumberRow extends StatefulWidget {
+  const NumberRow({Key? key}) : super(key: key);
 
+ 
+
+  @override
+  State<NumberRow> createState() => _NumberRowState();
+}
+
+class _NumberRowState extends State<NumberRow> {
+String valueChoose ;
+  List listitem=["item 1","item 2"];
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: const [],
+      children:  [
+        Container(
+          child: DropdownButton(
+            items:,
+            value:valueChoose ,
+            onChanged: (newValue){
+              setState(() {
+                valueChoose=newValue;
+              });
+            } ,
+            ),
+        )
+      ],
     );
   }
 }
